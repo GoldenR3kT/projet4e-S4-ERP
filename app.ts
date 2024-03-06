9b4c500b59f38336f2ad0b9ff67ba02a6a93d2e8
Nicolas Toupin <88687413+Zeidxn@users.noreply.github.com>
2024-03-06 11:30:06 +0100

    modification css et app.ts

--- app.ts
+++ app.ts
@@ -6,26 +6,62 @@ const port = 3000;
 
 // chemin absolu vers le dossier 'html'
 const htmlPath = path.join(__dirname, 'src', 'html');
+const cssPath = path.join(__dirname, 'assets', 'css');
+
 
 // On indique à Express d'utiliser le moteur de vue ejs
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+app.use('/css', express.static(cssPath));
+
 
 //dossier des vues à utiliser
 app.set('views', htmlPath);
-
 // Gérer les requêtes GET à la racine
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(htmlPath, 'index.html'));
 });
 
-// Gérer les requêtes GET pour la route "cash_desk"
+// Gérer les requêtes GET pour chaque page
 app.get('/cash_desk', (req, res) => {
-    res.sendFile(path.join(htmlPath+"/cash_desk", 'cash_desk.html'));
+    res.sendFile(path.join(htmlPath, 'cash_desk', 'cash_desk.html'));
 });
 
+app.get('/customer_cards', (req, res) => {
+    res.sendFile(path.join(htmlPath, 'customer_cards', 'customer_cards.html'));
+});
 
-//On lance le serveur sur le port 3000
+app.get('/help', (req, res) => {
+    res.sendFile(path.join(htmlPath, 'help', 'help.html'));
+});
+
+app.get('/human_resources', (req, res) => {
+    res.sendFile(path.join(htmlPath, 'human_resources', 'human_resources.html'));
+});
+
+app.get('/incidents', (req, res) => {
+    res.sendFile(path.join(htmlPath, 'incidents', 'incidents.html'));
+});
+
+app.get('/management', (req, res) => {
+    res.sendFile(path.join(htmlPath, 'management', 'management.html'));
+});
+
+app.get('/profile', (req, res) => {
+    res.sendFile(path.join(htmlPath, 'profile', 'profile.html'));
+});
+
+app.get('/schedule', (req, res) => {
+    res.sendFile(path.join(htmlPath, 'schedule', 'schedule.html'));
+});
+
+app.get('/storage', (req, res) => {
+    res.sendFile(path.join(htmlPath, 'storage', 'storage.html'));
+});
+
+// On lance le serveur sur le port 3000
 app.listen(port, () => {
     console.log(`Serveur démarré sur http://localhost:${port}`);
 });
