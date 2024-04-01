e3c90edbf9e09eec7f818b3d160aac70718800fc
Vizio Luca <luca.vizio@etu.uni-cotedazur.fr>
2024-04-01 23:09:32 +0200

    Ajout Vue profile-modifications

---
+++ src/ts/nav/navigation.js
@@ -0,0 +1,12 @@
+var navigationDiv = document.createElement('div');
+navigationDiv.id = 'navigation-div';
+var iframe = document.createElement('iframe');
+iframe.src = '/navigation';
+iframe.width = '18%';
+iframe.height = '100%';
+iframe.style.position = 'fixed';
+iframe.style.zIndex = '1000';
+navigationDiv.appendChild(iframe);
+var erpElement = document.getElementById('erp');
+navigationDiv.classList.add('slide-in');
+erpElement.insertBefore(navigationDiv, erpElement.firstChild);
