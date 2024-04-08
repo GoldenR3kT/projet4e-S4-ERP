55b4d640ba88f50a50d2a3b02167e6b8bba9e5c7
Axel Derail--Santer <axel.derail--santer@etu.unice.fr>
2024-04-08 11:55:58 +0200

    correction gerer incident

--- src/ts/database/db_queries.ts
+++ src/ts/database/db_queries.ts
@@ -101,6 +101,7 @@ export async function declarerIncident(nom: string, description: string, niveau:
 // Gérer un incident
 export async function gererIncident(id_incident: number, description: string, idEmploye: number, date: Date, heure: string): Promise<void> {
     await SolutionIncident.create({ id_incident: id_incident, desc: description, employe_id: idEmploye, date, heure });
+    await Incident.update({ en_cours: 0 }, { where: { incident_id: id_incident } });
 }
 
 // Voir tout les incidents
