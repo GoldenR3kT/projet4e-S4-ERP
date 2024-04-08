f1a7288b643bb4cf304af0faac999d5ba68426b0
Axel Derail--Santer <axel.derail--santer@etu.unice.fr>
2024-04-08 13:38:36 +0200

    fix modele mouvement

--- src/ts/database/db_models.ts
+++ src/ts/database/db_models.ts
@@ -260,8 +260,8 @@ const Mouvement = sequelize.define('mouvement', {
   onDelete: 'CASCADE'
 });
 
-Mouvement.belongsTo(Article, { foreignKey: 'article_id' });
-Mouvement.belongsTo(Transaction, { foreignKey: 'transaction_id' });
+Mouvement.belongsTo(Article, { foreignKey: 'id_client' });
+Mouvement.belongsTo(Transaction, { foreignKey: 'id_transaction' });
 
 const Carte = sequelize.define('carte', {
   num: {
