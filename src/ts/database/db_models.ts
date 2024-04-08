0572b9271667011f89e1fb65e66e71bad077ce2f
Axel Derail--Santer <axel.derail--santer@etu.unice.fr>
2024-04-08 15:12:00 +0200

    fix

--- src/ts/database/db_models.ts
+++ src/ts/database/db_models.ts
@@ -184,6 +184,7 @@ const Energie = sequelize.define('energie', {
 });
 
 Energie.belongsTo(Article, { foreignKey: 'id' });
+Article.has(Energie, { foreignKey: 'id' });
 
 const Produit = sequelize.define('produit', {
   image: DataTypes.STRING(100),
@@ -195,6 +196,7 @@ const Produit = sequelize.define('produit', {
 });
 
 Produit.belongsTo(Article, { foreignKey: 'id' });
+Article.has(Produit, { foreignKey: 'id' });
 
 const Menu = sequelize.define('menu', {
   id: {
